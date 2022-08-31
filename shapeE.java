b3db62dbab7b21f5910e9bef1314eb8ca2143d15
elifnurcansiz <81492504+elifnurcansiz@users.noreply.github.com>
2022-08-31 16:41:52 +0300

    java.CH.06.Making.Shape

---
+++ shapeE.java
@@ -0,0 +1,54 @@
+public class shapeE {
+    public static void main(String[] args) {
+        for(int i=0;i<7;i++){
+
+            if (i==0){
+                for(int j=0;j<7;j++){
+                System.out.print("*");
+
+                }
+            }
+            if (i==3){
+                for(int j=0;j<7;j++){
+                    System.out.print("*");}
+            }
+            if (i==6){
+                for(int j=0;j<7;j++){
+                    System.out.print("*");}
+            }
+            System.out.println("*");
+        }
+        System.out.println(" ");
+        for(int i=0;i<7;i++){
+            if (i==6){
+                for(int j=0;j<7;j++){
+                    System.out.print("*");}
+            }
+            System.out.println("*");
+        }
+        System.out.println(" ");
+        for(int i=0;i<7;i++){
+            System.out.println("*");
+            if (i==1){
+                System.out.println(" ");
+
+            }
+        }
+        System.out.println(" ");
+        for(int i=0;i<7;i++){
+
+            if (i==0){
+                for(int j=0;j<7;j++){
+                    System.out.print("*");
+
+                }
+            }
+            if (i==3){
+                for(int j=0;j<7;j++){
+                    System.out.print("*");}
+            }
+            System.out.println("*");
+        }
+        System.out.println(" ");
+    }
+}
