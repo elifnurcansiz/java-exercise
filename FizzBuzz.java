1956243638cccf52cfcf4401fd8bcfba00cb45ab
elifnurcansiz <81492504+elifnurcansiz@users.noreply.github.com>
2022-09-02 17:11:50 +0300

    java.CH.06.game

---
+++ FizzBuzz.java
@@ -0,0 +1,30 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++){
+            if(i%3==0){
+                System.out.println("fizz");
+            }
+            else if (i%5==0){
+                System.out.println("Buzz");
+
+            }
+            else if (i%15==0){
+                System.out.println("FizzBuzz");
+            }
+           else  {
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
